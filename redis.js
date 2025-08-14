9b0b514873df6ed04ccfb7fe549fa9da8f340179
jarot571 <jaron.t@epic-consulting.net>
2025-08-14 17:53:27 +0700

    Update redis.js

--- redis.js
+++ redis.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 
 const redisClient = redis.createClient({
   socket: {
-    host: process.env.REDIS_HOST || '127.0.0.1', //kubernetes Service Name
+    host: process.env.REDIS_HOST || '127.0.0.1', 
     port: process.env.REDIS_PORT || 6379, //default Port Redis
   }
 });
